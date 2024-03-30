a17c53921f790cebb7be1ec4c3013e16f1bd0d3e
kavinm <30965883+kavinm@users.noreply.github.com>
2024-03-30 02:53:39 -0400

    finished ERC777 markdown

--- Bonding-Curves-Token-Security/src/ERC20Sanction.sol
+++ Bonding-Curves-Token-Security/src/ERC20Sanction.sol
@@ -1,15 +1,29 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.21;
-import "@openzeppelin/contracts@4.9.3/token/ERC20/ERC20.sol";
+pragma solidity 0.8.20;
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Counter {
-    uint256 public number;
-
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+contract SanctionedToken is ERC20 {
+    address public _admin;
+    mapping(address => bool) _blacklist;
+    /**
+     * @dev Init constructor for setting token name and symbol
+     */
+    constructor() ERC20("Sanctioned Token", "SNT") {
+        _admin = msg.sender;
+        _mint(_admin, 1000);
     }
 
-    function increment() public {
-        number++;
+    function addAddressToBlacklist(address _toBeBlacklisted) public {
+        require(msg.sender == _admin);
+        _blacklist[_toBeBlacklisted] = true;
+    }
+
+    function _update(
+        address from,
+        address to,
+        uint256 value
+    ) internal virtual override {
+        ERC20._update(from, to, value);
+        require(_blacklist[from] == false && _blacklist[to] == false);
     }
 }
